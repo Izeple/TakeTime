7ba7958ab0dc72403a43f45fcfd65a162f98b36f
saranya2542 <saranya2542@gmail.com>
2019-11-22 22:20:37 +0700

    gew

--- chat.php
+++
@@ -1,4 +0,0 @@
-<?php
-$staffid = $_GET['staffid'];
-//echo $staffid;
-?>
--- insertchat.php
+++ insertchat.php
@@ -13,9 +13,7 @@ $MonthExpired = $_POST["MonthExpired"];
 $YearExpired = $_POST["YearExpired"];
 $CVV = $_POST["CVV"];
 $staffid = $_POST["staffid"];
-
-$CardNumber = $CardNumber1.$CardNumber2.$CardNumber3.$CardNumber4; 
-echo $staffid;
+$CardNumber = $CardNumber1 . $CardNumber2 . $CardNumber3 . $CardNumber4;
 $target_dir = "C:\AppServ\www\SE2\uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -54,18 +52,126 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
 } else {
-    $newname = rand(0,9999);
+    $newname = rand(0, 9999);
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir .  $newname  . $imageFileType)) {
-       // echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+        // echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
     } else {
-       // echo "Sorry, there was an error uploading your file.";
+        // echo "Sorry, there was an error uploading your file.";
     }
-    $namefile = $newname.".". $imageFileType;
+    $namefile = $newname . "." . $imageFileType;
 }
 $sql = "INSERT INTO `Consult` (`consult_id`, `staff_id`, `patient_id`, `consult_detail`, `howlong`, `unitlong`, `often`, `CardNumber`, `CVV`, `MonthExpired`, `YearExpired`, `img`)  
 	VALUES (NULL,'$staffid','1','$describe','$long','$unitlong','$often','$CardNumber','$CVV','$MonthExpired','$YearExpired','$namefile')";
-   if ($Connect->query($sql) === TRUE) {  
-    header("location: car.php");
-   } else {
-	   echo "Error: " . $sql . "<br>" . $Connect->error;
-   }
+if ($Connect->query($sql) === TRUE) {
+    // header("location: car.php");
+} else {
+    echo "Error: " . $sql . "<br>" . $Connect->error;
+}
+?>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        /* The Modal (background) */
+        .modal {
+            display: none;
+            /* Hidden by default */
+            position: fixed;
+            /* Stay in place */
+            z-index: 1;
+            /* Sit on top */
+            padding-top: 100px;
+            /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%;
+            /* Full width */
+            height: 100%;
+            /* Full height */
+            overflow: auto;
+            /* Enable scroll if needed */
+            background-color: rgb(0, 0, 0);
+            /* Fallback color */
+            background-color: rgba(0, 0, 0, 0.4);
+            /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: auto;
+            padding: 20px;
+            margin-top: 170px;
+            border: 1px solid #888;
+            width: 35%;
+        }
+
+        /* The Close Button */
+        .close {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        
+        .buttonconfirm {
+            background-color: #47b6c7;
+            border: none;
+            color: #ffffff;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 18px;
+            margin: 4px 2px;
+            cursor: pointer;
+            font-family: Agency FB;
+            border-radius: 5px;
+            height: 5%;
+            width: 8%;
+        }
+    </style>
+</head>
+<script>
+    function myFunction() {
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+    }
+</script>
+
+<body onload="myFunction()">
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <form action="chat.php" method="POST">
+            <p>    &nbsp;&nbsp;&nbsp;<font size='6' color="#a4a4a4" face="Agency FB"> Success Financial Transactions.</font></p>
+            &nbsp;&nbsp;&nbsp;<input type="submit" class="buttonconfirm" value="OK" >
+        </div>
+    </div>
+
+    <script>
+        var span = document.getElementsByClassName("close")[0];
+        span.onclick = function() {
+            window.open('chat.php', "_self");
+        }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                window.open('chat.php', "_self");
+            }
+        }
+    </script>
+
+</body>
+
+</html>

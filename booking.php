ee7a8110c52ddabf17eb94ebe36a2c639dc3d31d
Izeple <Izeple@hotmail.co.th>
2019-11-22 15:35:37 +0700

    edit select and booking

--- booking.php
+++ booking.php
@@ -25,20 +25,20 @@ if ($Connect->connect_error)
 
     <div id="main">
         <ul>
-            <li><a class="active" href="#home" onclick="clickNav()"><img src="./img/menu.png" height="15"></a></li>
-            <li><a href="#news"><img src="./img/nametag.png" height="15"></a></li>
-            <li style="float:right"><button class="btn" id="btn">Sign up</button></li>
-            <li style="float:right"><button class="btn2" id="btn2">Log in</button></li>
+       <li><a class="active" href="#home" onclick="clickNav()"><img src="./img/menu.png" height="15"></a></li>
+       <li><a href="#news"><img src="./img/nametag.png" height="15"></a></li>
+       <li style="float:right"><button class="btn" id="btn">Sign up</button></li>
+       <li style="float:right"><button class="btn2" id="btn2">Log in</button></li>
         </ul>
 
         <section><img src="./img/Banner.png" style="width:100%">
         <div class="ab" align="center">
-            <div id="sidenav" class="sidenav">
+       <div id="sidenav" class="sidenav">
    <div class="sidein"><a href="homepage.php"><img src="img/user.png" height="30"></a></div>
    <div class="sidein"><a href="selectdoc.php"><img src="img/help.png" height="30"></a></div>
    <div class="sidein"><a href="booking.php"><img src="img/time.png" height="30"></a></div>
    <div class="sidein"><a href="test.php"><img src="img/noti.png" height="30"></a></div>
-            </div>
+       </div>
         </div>
         </section>
     </div>    
@@ -48,28 +48,53 @@ if ($Connect->connect_error)
 
    booking
    <?php 
+   session_start();
    require_once("connectPDO.php");
-   $pdo = conPDO();    
-   $HospitalID = 1; 
-   $DepartmentID = 0;
+   $pdo = conPDO();     
+   if(isset($_POST["HospitalID_Select"]) || !isset($_SESSION['HospitalID_Select']))
+   $_SESSION['HospitalID_Select']= isset($_POST["HospitalID_Select"]) ? $_POST["HospitalID_Select"] : NULL; 
+   if(isset($_POST["DepartmentID_Select"]) || !isset($_SESSION['DepartmentID_Select']))
+   $_SESSION['DepartmentID_Select']= isset($_POST["DepartmentID_Select"]) ? $_POST["DepartmentID_Select"] : NULL; 
+
+   $HospitalID = $_SESSION['HospitalID_Select'];
+   $DepartmentID = $_SESSION['DepartmentID_Select'];
+   echo "<br>HospitalID =",$HospitalID;
+   echo "<br>DepartmentID =",$DepartmentID;
    ?>
 
    <form style="display: inline; " action='<?php echo $_SERVER['PHP_SELF']; ?>' method="POST">     
-          <select name="BranchID_Select" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Select BranchID
-              <option value=""><- Select DepartmentID -></option>
+        <select name="HospitalID_Select" type="button">
+            <option value=""><- Select hospitalID -></option>
+                <?php
+                $result_H = $pdo->prepare("SELECT * FROM hospital");
+                $result_H->execute();
+                while($objResuut = $result_H->fetch()){
+                echo print_r($objResuut);
+                ?>
+                <option value="<?php echo $objResuut["hospital_id"];?>" <?php if ($objResuut["hospital_id"] == $HospitalID) echo ' selected="selected"';?>><?php echo $objResuut["hospital_id"]." - ".$objResuut["hospital_name"];?></option>
+                <?php } ?>
+        </select>
+    </form>
+
+    <form style="display: inline; " action='<?php echo $_SERVER['PHP_SELF']; ?>' method="POST">     
+        <select name="DepartmentID_Select" type="button">
+            <option value=""><- Select DepartmentID -></option>
                 <?php
                 $result_B = $pdo->prepare("SELECT * FROM department WHERE hospital_id=:HospitalID");
                 $result_B->execute(array(':HospitalID' => $HospitalID));
                 while($objResuut = $result_B->fetch()){
-                    echo print_r($objResuut);
+                echo print_r($objResuut);
                 ?>
                 <option value="<?php echo $objResuut["department_id"];?>" <?php if ($objResuut["department_id"] == $DepartmentID) echo ' selected="selected"';?>><?php echo $objResuut["department_id"]." - ".$objResuut["department_name"];?></option>
                 <?php } ?>
-                </select>
-          </div>
+        </select>
     </form>
 
+    
+    
+    
+    
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 
 

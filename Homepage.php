87282325370b8766e370ad238ca2de732f677fd0
Izeple <Izeple@hotmail.co.th>
2019-11-22 13:58:36 +0700

    script

--- Homepage.php
+++ Homepage.php
@@ -18,6 +18,7 @@ if ($Connect->connect_error)
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="./css/home.css">
     <link rel="stylesheet" type="text/css" href="http://allfont.net/allfont.css?fonts=agency-fb"/>
+    <script type="text/javascript" src="./script/clickNav.js"></script>
     <title>Homepage</title>
 </head>
 <body>
@@ -64,60 +65,16 @@ if ($Connect->connect_error)
     
 
     <div id="logpop" class="login">
-
             <!-- Modal content -->
             <div class="log-content">
               <span class="close">&times;</span>
                 <br>
               <input type="text" name="Email" value="Email"><br>
               <input type="password" name="Password" value="Password">
-
-
             </div>
-          
     </div>
 
 
 
-
-
-
-
-
-    
-        
-    <script>
-            function clickNav(){
-                console.log(document.getElementById("sidenav").style.width);
-                if(document.getElementById("sidenav").style.width == "250px")
-                {
-                    document.getElementById("sidenav").style.width = "0%";
-                }
-                else
-                {
-                    document.getElementById("sidenav").style.width = "250px";
-                }
-            }
-
-            var modal = document.getElementById("logpop");
-
-            // Get the button that opens the modal
-            var btn = document.getElementById("btn2");
-
-             // Get the <span> element that closes the modal
-            var span = document.getElementsByClassName("close")[0];
-
-            // When the user clicks the button, open the modal 
-            btn.onclick = function() {
-            modal.style.display = "block";
-            }
-
-            // When the user clicks on <span> (x), close the modal
-            span.onclick = function() {
-            modal.style.display = "none";
-}
-    </script>
 </body>
-
-    
 </html>
---
+++ booking.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "projectdead";
+
+$Connect = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($Connect,"utf8");
+if ($Connect->connect_error)
+{
+	die("Connection failed: ". $Connect->connect_error);
+}
+?>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./css/home.css">
+    <link rel="stylesheet" type="text/css" href="http://allfont.net/allfont.css?fonts=agency-fb"/>
+    <script type="text/javascript" src="./script/clickNav.js"></script>
+    <title>Homepage</title>
+</head>
+<body>
+
+    <div id="main">
+        <ul>
+            <li><a class="active" href="#home" onclick="clickNav()"><img src="./img/menu.png" height="15"></a></li>
+            <li><a href="#news"><img src="./img/nametag.png" height="15"></a></li>
+            <li style="float:right"><button class="btn" id="btn">Sign up</button></li>
+            <li style="float:right"><button class="btn2" id="btn2">Log in</button></li>
+        </ul>
+
+        <section><img src="./img/Banner.png" style="width:100%">
+        <div class="ab" align="center">
+            <div id="sidenav" class="sidenav">
+                <div class="sidein"><a href="homepage.php"><img src="img/user.png" height="30"></a></div>
+                <div class="sidein"><a href="selectdoc.php"><img src="img/help.png" height="30"></a></div>
+                <div class="sidein"><a href="booking.php"><img src="img/time.png" height="30"></a></div>
+                <div class="sidein"><a href="test.php"><img src="img/noti.png" height="30"></a></div>
+            </div>
+        </div>
+        </section>
+    </div>    
+
+    
+
+    <div id="logpop" class="login">
+            <!-- Modal content -->
+            <div class="log-content">
+              <span class="close">&times;</span>
+                <br>
+              <input type="text" name="Email" value="Email"><br>
+              <input type="password" name="Password" value="Password">
+            </div>
+    </div>
+</body>
+</html>

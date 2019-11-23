b766ccb6712c77ce68b4fc19988daa260d90df0f
Izeple <Izeple@hotmail.co.th>
2019-11-23 21:13:29 +0700

    Update Select doc

--- selectdoc.php
+++ selectdoc.php
@@ -19,194 +19,33 @@
     </script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome Icon Library -->
+    <link rel="stylesheet" type="text/css" href="./css/home.css">
     <link rel="stylesheet" type="text/css" href="./css/selectdoc.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        input[type=text],
-        select {
-            width: 160px;
-            padding: 12px 20px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            box-sizing: border-box;
-            padding: 10px;
-            color: #a4a4a4;
-        }
-
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            transition: 0.3s;
-            width: 420px;
-            height: 140px;
-            overflow: auto;
-        }
-
-        .card1 {
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-            transition: 0.3s;
-            width:  250px;
-            height: 290px;
-            overflow: auto;
-        }
-
-        .card:hover {
-            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
-        }
-
-        .container {
-            padding: 2px 16px;
-        }
-
-        .img1 {
-            float: right;
-        }
-
-        .img2 {
-            float: left;
-        }
-
-        .fa {
-            font-size: 25px;
-            color: #8c8c8c;
-        }
-
-        .checked {
-            color: #efce4a;
-        }
-
-        .row {
-            display: flex;
-            margin-left: 25%;
-            margin-right: auto;
-        }
-
-        .column {
-            padding: 10px;
-        }
-
-        .row1 {
-            display: flex;
-        }
-
-        .column1 {
-            margin: auto;
-        }
-
-        .modal {
-            Position: relative;
-            display: none;
-            /* Hidden by default */
-            position: fixed;
-            /* Stay in place */
-            z-index: 1;
-            /* Sit on top */
-            padding-top: 10px;
-            /* Location of the box */
-            left: 0;
-            top: 0;
-            width: 100%;
-            /* Full width */
-            height: 100%;
-            /* Full height */
-            overflow: auto;
-            /* Enable scroll if needed */
-            background-color: rgb(0, 0, 0);
-            /* Fallback color */
-            background-color: rgba(0, 0, 0, 0.4);
-            /* Black w/ opacity */
-
-        }
-
-        /* Modal Content */
-        .modal-content {
-            Position: relative;
-            background-color: #fefefe;
-            width: 62%;
-            margin: auto;
-            padding: 10px;
-            border-radius: 10px;
-        }
-
-        /* The Close Button */
-        .close {
-            color: #aaaaaa;
-            float: right;
-            font-size: 28px;
-            font-weight: bold;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: #000;
-            text-decoration: none;
-            cursor: pointer;
-        }
-
-        textarea {
-            resize: none;
-            border: 1px solid #d3cccc;
-            font-size: 16px;
-        }
-
-        input[type="number"] {
-            text-align: center;
-            border-style: solid;
-            border-color: #d3d3d3;
-        }
-
-        .buttoncancel {
-            background-color: #e1e1e1;
-            border: none;
-            color: #475254;
-            padding: 10px 20px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 18px;
-            margin: 4px 2px;
-            cursor: pointer;
-            font-family: Agency FB;
-            border-radius: 5px;
-        }
-
-        .buttonconfirm {
-            background-color: #47b6c7;
-            border: none;
-            color: #ffffff;
-            padding: 10px 20px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 18px;
-            margin: 4px 2px;
-            cursor: pointer;
-            font-family: Agency FB;
-            border-radius: 5px;
-        }
-
-        .upload-btn-wrapper {
-            position: relative;
-            overflow: hidden;
-            display: inline-block;
-        }
-
-        .upload-btn-wrapper input[type=file] {
-            font-size: 100px;
-            position: absolute;
-            left: 0;
-            top: 0;
-            opacity: 0;
-        }
-    </style>
+    <script type="text/javascript" src="./script/clickNav.js"></script>
+    <?php require "condb.php"; ?>
 </head>
 
 <body>
-    <header>
-        <img type="image" src="./img/header.jpg" style="width:99%; height: 305px;">
-    </header>
-    <?php require "condb.php"; ?>
+    <div id="main">
+        <ul>
+            <li><a class="active" href="#home" onclick="clickNav()"><img src="./img/menu.png" height="15"></a></li>
+            <li><a href="#news"><img src="./img/nametag.png" height="15"></a></li>
+            <li style="float:right"><button class="btn" id="btn">Sign up</button></li>
+            <li style="float:right"><button class="btn2" id="btn2">Log in</button></li>
+        </ul>
+
+        <section><img src="./img/Banner.png" style="width:100%">
+        <div class="ab" align="center">
+            <div id="sidenav" class="sidenav">
+                    <div class="sidein"><a href="homepage.php"><img src="img/user.png" height="30"></a></div>
+                    <div class="sidein"><a href="selectdoc.php"><img src="img/help.png" height="30"></a></div>
+                    <div class="sidein"><a href="booking_0.php"><img src="img/time.png" height="30"></a></div>
+                    <div class="sidein"><a href="Notification.php"><img src="img/noti.png" height="30"></a></div>
+            </div>
+        </div>
+        </section>
+    </div>    
     <br>
 
     <div id="myModal" class="modal">

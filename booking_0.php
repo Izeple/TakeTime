4e55b183955ac2796a37e9a54c2716e73f2a6674
Izeple <Izeple@hotmail.co.th>
2019-11-24 14:28:42 +0700

    booking [doing]

--- booking_0.php
+++ booking_0.php
@@ -4,13 +4,12 @@
     {
         $email = $_SESSION["email"];
         require_once("connectPDO.php");
-        $pdo = conPDO();
         $sql = "SELECT * FROM patient WHERE email = '".$email."'";
         $result_User = PDOfetchAll($sql)[0];
     }
+    else
+        header("location:Homepage.php");
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
@@ -28,15 +27,15 @@
     <div id="main">
         <ul>
             <li><a class="active" href="#home" onclick="clickNav()"><img src="./img/menu.png" height="15"></a></li>
-            <li><a href="#news"><img src="./img/nametag.png" height="15"></a></li>
-            <li style="float:right"><button class="btn" id="btn">Sign up</button></li>
-            <li style="float:right"><button class="btn2" id="btn2">Log in</button></li>
+            <li><a href="Homepage.php"><img src="./img/nametag.png" height="15"></a></li>
+            <li style="float:right"><button class="btn2" id="btn3" onclick="location.replace('./logout.php');">Logout</button></li>
+            <li style="float:right"><?php echo $result_User["name"]; ?></li>
         </ul>
 
         <section><img src="./img/Banner.png" style="width:100%">
         <div class="ab" align="center">
             <div id="sidenav" class="sidenav">
-                    <div class="sidein"><a href="homepage.php"><img src="img/user.png" height="30"></a></div>
+                    <div class="sidein"><a href="profile.php"><img src="img/user.png" height="30"></a></div>
                     <div class="sidein"><a href="selectdoc.php"><img src="img/help.png" height="30"></a></div>
                     <div class="sidein"><a href="booking_0.php"><img src="img/time.png" height="30"></a></div>
                     <div class="sidein"><a href="Notification.php"><img src="img/noti.png" height="30"></a></div>
@@ -45,10 +44,33 @@
         </section>
     </div>  
     
+
+
+
+    <?php //'".$result_User['patient_id']."'
+        $sql = "SELECT * FROM schedule WHERE patient_id = 1";
+        $result_Schedule = PDOfetchAll($sql);
+        if(!$result_Schedule)
+        {
+            echo "<center>";
+            echo "<img src='./img/add.jpg' style='width:100px;'><br>";
+            echo "</center>";
+        }
+        else
+        {
+            foreach ($result_Schedule as $row) {?>
+            <?php echo $row['bookingdate'];?><br>
+            <?php echo $row['staff_id'];?><br>
+            <?php echo $row['detail'];?><br>
+            <?php echo $row['status'];?><br>      
+        
+        <?php
+            }
+        }
+    ?>
     
 
 
-    
 
 
 

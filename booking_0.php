4b2a181a8f169a42e940f5efd12f1e50e2ed234a
Izeple <Izeple@hotmail.co.th>
2019-11-26 11:48:29 +0700

    Update Booking 90%

--- booking_0.php
+++ booking_0.php
@@ -46,53 +46,64 @@
 
     
     <h1 class="HeadModule">Booking Doctor</h1>
-    <h2 onclick="document.getElementById('logpop').style.display='block'" class="HeadModule-h2-1">Booking</h2>
-    <h2 onclick="document.getElementById('logpop').style.display='block'" class="HeadModule-h2-2">Complete</h2>
-    
+    <h2 onclick="location.replace('./booking_0.php');" class="HeadModule-h2-1">Booking</h2>
+    <h2 onclick="location.replace('./booking_0c.php');" class="HeadModule-h2-2">Complete</h2>
 
-    <?php //'".$result_User['patient_id']."'
-        $sql = "SELECT * FROM schedule WHERE patient_id = 1";
-        $result_Schedule = PDOfetchAll($sql);
-        if(!$result_Schedule)
-        {
-            
-        }
-        else
-        {
-            foreach ($result_Schedule as $row) {?>
-
-            <?php if($row['status']=="Complete") { ?>
-            <h4 class="HeadModule-h4-1">Booking</h4>     
-            <div id="complete" class="card"  style="margin-top:60px; margin-left:100px;">
-                <img src="./img/picdoc.jpg" alt="Avatar" style="width:140px;" class="img2">
-                
-
-                <font size='5' color="#47b6c7"> 
-                    Dr. <?php  
-                    $sql = "SELECT * FROM staff WHERE staff_id = '".$row['staff_id']."'";
-                    $result_doctor = PDOfetchAll($sql)[0];
-                    echo $result_doctor['name'],$result_doctor['surname'];?><br>
-                </font>
-
-                <font size='3' color="#a4a4a4">
-                    <?php $dates = explode(' ', $row['bookingdate']); ?>
-                    Date <?php echo $dates[0];?><br>
-                    Time <?php echo $dates[1];?><br>
-
-                    Hospital <?php  
-                    $sql = "SELECT * FROM hospital WHERE hospital_id = '".$result_doctor['hospital_id']."'";
-                    $result_Hospital = PDOfetchAll($sql)[0];
-                    echo $result_Hospital['hospital_name']?><br>
-                </font>
-
-                <br>    
-            </div>  
+    <div class="row" style="margin-left:-20px;">
         <?php
+            $status_select="Going on";
+            $i = 0;
+            //'".$result_User['patient_id']."'
+            $sql = "SELECT * FROM schedule WHERE patient_id = 1";
+            $result_Schedule = PDOfetchAll($sql);
+            if($result_Schedule)
+            {
+                foreach ($result_Schedule as $row) {
+                    if ($i % 2 == 0 && $i != 0) {
+                    
+                    ?>
+        </div>
+        <div class="row" style="margin-left:-20px;">
+        <?php } ?>
+            <?php if($row['status']==$status_select && $row['status']=="Going on") { ?>
+                <div class="column" >
+                    <div class="card"  style="margin-top:60px; margin-left:100px;">
+                        <div class="side right" align="left">
+                            <h4 class="HeadModule-h4-1" style="position: absolute; margin-top:-12px; margin-left:-7px;">Going on</h4>
+                            <img src="./img/picdoc.jpg" alt="Avatar" style="width:140px;" class="img2">
+    
+                            <font size='5' color="#47b6c7"> 
+                                Dr. <?php  
+                                $sql = "SELECT * FROM staff WHERE staff_id = '".$row['staff_id']."'";
+                                $result_doctor = PDOfetchAll($sql)[0];
+                                echo $result_doctor['name'],$result_doctor['surname'];?><br>
+                            </font>
+    
+                            <font size='3' color="#a4a4a4">
+                                <?php $dates = explode(' ', $row['bookingdate']); ?>
+                                Date <?php echo $dates[0];?><br>
+                                Time <?php echo $dates[1];?><br>
+    
+                                Hospital <?php  
+                                $sql = "SELECT * FROM hospital WHERE hospital_id = '".$result_doctor['hospital_id']."'";
+                                $result_Hospital = PDOfetchAll($sql)[0];
+                                echo $result_Hospital['hospital_name']?><br>
+                            </font>
+                            <form style="position: absolute; margin-top:-100px; margin-left:397px;" action="delete_booking.php" method="POST">
+                                <input type='submit' value='x' onclick="return confirm('Are you sure to Delete?')">
+                            </form>
+                            <button class="btn" id="btn" onclick="document.getElementById('logpop').style.display='block'">Edit</button></li>
+                            <br> 
+                        </div>   
+                    </div> 
+                </div>  
+                <?php
+                  $i++;   
                 }
-            }
+             }
         }
-        $_SESSION['staff_id'] = $result_doctor['staff_id'];
     ?>
+    </div>  
 
     <center>
         <a href="booking_1.php" ><img src='./img/add.jpg' style='width:100px;'><br></a>
@@ -104,6 +115,28 @@
 
 
 
+    <div id="logpop" class="login">
+            <!-- Modal content -->
+            <div class="log-content">
+              <span class="close" onclick="document.getElementById('logpop').style.display='none'">&times;</span>
+              <div class="sep"><p style="color: #6690a0; font-size:40px; text-align:center; margin:10px;">Login</p></div>
+              <form class="user" method="post">
+                <div class="sep"><p>Email</p><span class="required">*</span></div>
+                <div class="sep"><input type="email" name="email"></div>
+                <div class="sep"><p>Password</p><span class="required">*</span></div>
+                <div class="sep"><input type="password" name="password"></div>
+                <div class="sep" style="width:50%; display:inline-block;"><input type="checkbox" checked="checked" name="remember"> <a style="font-size:18px;">Remember me</a></div>
+                <div class="sep" style="width:43%; text-align:right; display:inline-block;"><a class="aa" href="#" style="font-size:18px; text-decoration:none;">Forget Password ?</a></div>
+                <div class="sep"><input type="submit" name="login" class="sub" value="Login" /> </div>
+              </form> 
+              <div class="sep" style="margin-top: 1rem; margin-bottom: 1rem;"><div class="line"></div></div>
+              <div class="sep"><button class="face">Facebook</button><button class="goog">Google</button></div>
+            </div>
+    </div>
+
+
 
 </body>
 </html>
+
+

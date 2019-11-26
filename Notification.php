29353e99ef34dc02ff4ee770abfe76c2c7ce5d44
aummua111 <gameyoyolnw@gmail.com>
2019-11-26 17:51:38 +0700

    update noti

--- Notification.php
+++ Notification.php
@@ -129,6 +129,87 @@
 			}
 		} ?>
 
+							<!--Set time noti-->
+					<div style="position: absolute; margin-top: -170;">
+						<!--Set time noti eat-->
+							<div  style=" height: 345px;width: 1050px; background-color: #E9E9E9;border-radius: 5px; margin-left: 700px;margin-bottom: 50px; "> 
+								<div style="float: left;height: 345px;width: 1050px;">
+									
+							<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 20;margin-left: 100;"> 
+									<img src="./img/sunrise (2).png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
+									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Morning"; ?></div>
+								</div>
+								
+									<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 30;margin-left: 100;"> 
+									<img src="./img/sunset.png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
+									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Evening"; ?></div>
+								</div>
+										<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: -305;margin-left: 550;"> 
+									<img src="./img/sun (1).png" style="width:95px; margin-top: 10px; margin-left: 20px;" >
+									<div style="margin-left:50;"><font size='5' color="#828282"><?php echo "Noon"; ?></div>
+								</div>
+										<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 25;margin-left: 550;"> 
+							<img src="./img/sleep.png"  style="width:85px; margin-top: 13px; margin-left: 30px;" >
+									<div style="margin-top: 5;margin-left:50;"><font size='5' color="#828282"><?php echo "Night"; ?></div>
+								</div>
+						</div>
+
+					
+					</div>
+						
+						<!--Infomation Noti-->
+					<div  style=" height: 345px;width: 1050px; background-color: #E9E9E9;border-radius: 5px; margin-left: 700px;"> 
+						<!--Infomation Noti Left Side-->
+						 <div><img src="./img/pills.png" style="width:130px; margin-top: 35px; margin-left: 30px;">	
+							 <div style="width:500px;margin-top: -120px; margin-left: 190px"><font size='7' color="#828282" >
+                               <?php echo "NAME: Paracetamol"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+					</div>	
+						<div style="margin-top: 20px; margin-left: 190;width:500px;"><font size='6' color="#828282" style="">
+                               <?php echo "Times: 12 times"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+				<div >
+						<div  style="border: 2px solid #D5D5D5; height: 55px;width: 120px; background-color: whitesmoke;border-radius: 5px; margin-left: 210px;margin-top: 40px;"> 
+							<div style="margin-left: 25;margin-top: 8"><font size='6' color="#828282"><?php echo "Before"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+					</div>
+						<div  style="border: 2px solid #D5D5D5; height: 55px;width: 120px; background-color: whitesmoke;border-radius: 5px; margin-left: 360px;margin-top: -59px;"> 
+							<div style="margin-left: 35;margin-top: 8"><font size='6' color="#828282"><?php echo "After"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+					</div>
+				  <img src="./img/breakfast.png"  style="width:90px; margin-top: -100px; margin-left: 530px;" >	
+					<div style="width: 120px;margin-left:545;margin-top: 0"><font size='6' color="#828282"><?php echo "Meals"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+					</div>
+						<!--Time Noti Right Side-->
+						<div style="float: right; width: 350px; height: 320px;margin-top: -281px">
+								<!--Morning-->
+								<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 15;"> 
+									<img src="./img/sunrise (2).png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
+									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Morning"; ?></div>
+								</div>
+										<!--Noon-->
+								<div  style="border: 2px solid #D5D5D5;height: 135px; width: 135px; background-color: whitesmoke;border-radius: 5px; margin-left: 160;margin-top: -138;"> 
+									<img src="./img/sun (1).png" style="width:95px; margin-top: 10px; margin-left: 20px;" >
+									<div style="margin-left:50;"><font size='5' color="#828282"><?php echo "Noon"; ?></div>
+								</div>
+										<!--Evening-->
+								<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 10;"> 
+									<img src="./img/sunset.png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
+									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Evening"; ?></div>
+								</div>
+										<!--Night-->
+								<div  style="border: 2px solid #D5D5D5;height: 135px; width: 135px; background-color: whitesmoke;border-radius: 5px; margin-left: 160;margin-top: -138;"> 
+									<img src="./img/sleep.png"  style="width:85px; margin-top: 13px; margin-left: 30px;" >
+									<div style="margin-top: 5;margin-left:50;"><font size='5' color="#828282"><?php echo "Night"; ?></div>
+								</div>
+						</div>
+				</div>
+									<!--Botton-->
+									<div>
+										<div  style="border: 2px solid #f26d7d; height: 55px;width: 120px; background-color: #f26d7d;border-radius: 5px; margin-left:1400px;margin-top: 30px;"> 
+							<div style="margin-left: 33;margin-top: 7"><font size='6' color="white"><?php echo "Close"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+										</div>
+											<div  style="border: 2px solid #47b6c7; height: 55px;width: 120px; background-color: #47b6c7;border-radius: 5px; margin-left:1550px; margin-bottom: 40px;margin-top: -58"> 
+							<div style="margin-left: 36;margin-top: 7"><font size='6' color="white"><?php echo "Save"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
+											</div>
+									</div>
+			</div>
  
 			 <div class="notimenu" style="font-size: 30px">
 			 Medicine
@@ -143,7 +224,7 @@
 			$countResult = mysqli_query($Connect, $mysql_qryCount1);
 			$count = $countResult->fetch_assoc();
 			if ($count['count']!=0) {?>
-				 		<div style=" display: inline-block border: 2px solid #e3e7e7; margin-left: 150px;margin-top: 15px;margin-bottom: 10px;width: 463px;">
+				 		<div style=" border: 2px solid #e3e7e7; margin-left: 150px;margin-top: 15px;margin-bottom: 10px;width: 463px;">
 						<div style="margin-top: 10px;margin-bottom: 10px;"> <?php 
 				while ($medic =  $result2->fetch_assoc()) { ?>
                 <div class="column">
@@ -199,87 +280,7 @@
 			 <?php	
 		   }?>
 
-						<!--Set time noti-->
-					<div style="margin-top: -1100;">
-						<!--Set time noti eat-->
-							<div  style=" height: 345px;width: 1050px; background-color: #E9E9E9;border-radius: 5px; margin-left: 700px;margin-bottom: 50px; "> 
-								<div style="float: left;height: 345px;width: 1050px;">
-									
-							<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 20;margin-left: 100;"> 
-									<img src="./img/sunrise (2).png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
-									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Morning"; ?></div>
-								</div>
-								
-									<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 30;margin-left: 100;"> 
-									<img src="./img/sunset.png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
-									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Evening"; ?></div>
-								</div>
-										<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: -305;margin-left: 550;"> 
-									<img src="./img/sun (1).png" style="width:95px; margin-top: 10px; margin-left: 20px;" >
-									<div style="margin-left:50;"><font size='5' color="#828282"><?php echo "Noon"; ?></div>
-								</div>
-										<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 25;margin-left: 550;"> 
-							<img src="./img/sleep.png"  style="width:85px; margin-top: 13px; margin-left: 30px;" >
-									<div style="margin-top: 5;margin-left:50;"><font size='5' color="#828282"><?php echo "Night"; ?></div>
-								</div>
-						</div>
 
-					
-					</div>
-						
-						<!--Infomation Noti-->
-					<div  style="height: 345px;width: 1050px; background-color: #E9E9E9;border-radius: 5px; margin-left: 700px;"> 
-						<!--Infomation Noti Left Side-->
-						 <div><img src="./img/pills.png" style="width:130px; margin-top: 35px; margin-left: 30px;">	
-							 <div style="width:500px;margin-top: -120px; margin-left: 190px"><font size='7' color="#828282" >
-                               <?php echo "NAME: Paracetamol"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-					</div>	
-						<div style="margin-top: 20px; margin-left: 190;width:500px;"><font size='6' color="#828282" style="">
-                               <?php echo "Times: 12 times"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-				<div >
-						<div  style="border: 2px solid #D5D5D5; height: 55px;width: 120px; background-color: whitesmoke;border-radius: 5px; margin-left: 210px;margin-top: 40px;"> 
-							<div style="margin-left: 25;margin-top: 8"><font size='6' color="#828282"><?php echo "Before"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-					</div>
-						<div  style="border: 2px solid #D5D5D5; height: 55px;width: 120px; background-color: whitesmoke;border-radius: 5px; margin-left: 360px;margin-top: -59px;"> 
-							<div style="margin-left: 35;margin-top: 8"><font size='6' color="#828282"><?php echo "After"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-					</div>
-				  <img src="./img/breakfast.png"  style="width:90px; margin-top: -100px; margin-left: 530px;" >	
-					<div style="width: 120px;margin-left:545;margin-top: 0"><font size='6' color="#828282"><?php echo "Meals"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-					</div>
-						<!--Time Noti Right Side-->
-						<div style="float: right; width: 350px; height: 320px;margin-top: -281px">
-								<!--Morning-->
-								<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 15;"> 
-									<img src="./img/sunrise (2).png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
-									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Morning"; ?></div>
-								</div>
-										<!--Noon-->
-								<div  style="border: 2px solid #D5D5D5;height: 135px; width: 135px; background-color: whitesmoke;border-radius: 5px; margin-left: 160;margin-top: -138;"> 
-									<img src="./img/sun (1).png" style="width:95px; margin-top: 10px; margin-left: 20px;" >
-									<div style="margin-left:50;"><font size='5' color="#828282"><?php echo "Noon"; ?></div>
-								</div>
-										<!--Evening-->
-								<div  style="border: 2px solid #D5D5D5; height: 135px;width: 135px; background-color: whitesmoke;border-radius: 5px; margin-top: 10;"> 
-									<img src="./img/sunset.png"  style="width:110px; margin-top: 5px; margin-left: 10px;" >
-									<div style="margin-top: -12;margin-left:40;"><font size='5' color="#828282"><?php echo "Evening"; ?></div>
-								</div>
-										<!--Night-->
-								<div  style="border: 2px solid #D5D5D5;height: 135px; width: 135px; background-color: whitesmoke;border-radius: 5px; margin-left: 160;margin-top: -138;"> 
-									<img src="./img/sleep.png"  style="width:85px; margin-top: 13px; margin-left: 30px;" >
-									<div style="margin-top: 5;margin-left:50;"><font size='5' color="#828282"><?php echo "Night"; ?></div>
-								</div>
-						</div>
-				</div>
-									<!--Botton-->
-									<div>
-										<div  style="border: 2px solid #f26d7d; height: 55px;width: 120px; background-color: #f26d7d;border-radius: 5px; margin-left:1400px;margin-top: 30px;"> 
-							<div style="margin-left: 33;margin-top: 7"><font size='6' color="white"><?php echo "Close"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-										</div>
-											<div  style="border: 2px solid #47b6c7; height: 55px;width: 120px; background-color: #47b6c7;border-radius: 5px; margin-left:1550px; margin-bottom: 40px;margin-top: -58"> 
-							<div style="margin-left: 36;margin-top: 7"><font size='6' color="white"><?php echo "Save"; ?>  &nbsp;&nbsp;&nbsp;</font></div>
-											</div>
-									</div>
-			</div>
 									
 									
     <script>

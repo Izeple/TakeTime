072dd0e59ff3416025d38ba005f22e6060fef1d2
saranya2542 <saranya2542@gmail.com>
2019-11-26 22:37:37 +0700

    gew

--- insertchat.php
+++ insertchat.php
@@ -22,7 +22,6 @@ $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 if (isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if ($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -31,12 +30,10 @@ if (isset($_POST["submit"])) {
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
@@ -44,27 +41,25 @@ if (
     $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif"
 ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    $sql = "INSERT INTO `Consult` (`consult_id`, `staff_id`, `patient_id`, `consult_detail`, `howlong`, `unitlong`, `often`, `CardNumber`, `CVV`, `MonthExpired`, `YearExpired`, `img`, `star`, `answer`)  
-	VALUES (NULL,'$staffid','1','$describe','$long','$unitlong','$often','$CardNumber','$CVV','$MonthExpired','$YearExpired','-','0','')";
+    $sql = "INSERT INTO `Consult` (`consult_id`, `staff_id`, `patient_id`, `consult_detail`, `time frame`, `often`, `CardNumber`, `img`, `star`, `answer`)  
+    VALUES (NULL,'$staffid','1','$describe','$long.$unitlong','$often','$CardNumber','-','0','')";
     if ($Connect->query($sql) === TRUE) {
         // header("location: car.php");
     } else {
-        echo "Error: " . $sql . "<br>" . $Connect->error;
     }
 } else {
     $newname = rand(0, 9999);
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir .  $newname  . $imageFileType)) {
-        $sql = "INSERT INTO `Consult` (`consult_id`, `staff_id`, `patient_id`, `consult_detail`, `howlong`, `unitlong`, `often`, `CardNumber`, `CVV`, `MonthExpired`, `YearExpired`, `img`, `star`, `answer`)  
-        VALUES (NULL,'$staffid','1','$describe','$long','$unitlong','$often','$CardNumber','$CVV','$MonthExpired','$YearExpired','$namefile','0','')";
+        $sql = "INSERT INTO `Consult` (`consult_id`, `staff_id`, `patient_id`, `consult_detail`, `time frame`, `often`, `CardNumber`, `img`, `star`, `answer`)  
+        VALUES (NULL,'$staffid','1','$describe','$long.$unitlong','$often','$CardNumber','$namefile','0','')";
         if ($Connect->query($sql) === TRUE) {
             // header("location: car.php");
         } else {
-            echo "Error: " . $sql . "<br>" . $Connect->error;
+           // echo "Error: " . $sql . "<br>" . $Connect->error;
         }
     } else {
         // echo "Sorry, there was an error uploading your file.";

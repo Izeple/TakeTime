dbf5d10fc71a022d7cb84bb6725be24fc9c1ec1a
aummua111 <gameyoyolnw@gmail.com>
2019-11-27 03:31:50 +0700

    Update noti

---
+++ js/notiClick.js
@@ -0,0 +1,8 @@
+var i = 8;
+function clicked(n) {
+    var img = document.getElementsByTagName("img");
+    var hour = document.getElementById("hour");
+    var i = i + n;
+ test.innerHTML = i;
+};// JavaScript Document
+window.onclick;

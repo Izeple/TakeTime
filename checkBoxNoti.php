2b05096e6f16ee48c69b1389512fbec860c73c79
aummua111 <gameyoyolnw@gmail.com>
2019-11-30 17:50:32 +0700

    Fix bug user

--- checkBoxNoti.php
+++ checkBoxNoti.php
@@ -1,9 +1,10 @@
 <?php 
  $status = $_REQUEST["status"];
 $medicid = $_REQUEST["medicid"];
+$patient_id = $_REQUEST["userid"];
 require "condb.php";
  
-$sql = "UPDATE Notification SET status='".$status."' WHERE medicine_id='".$medicid."'";
+$sql = "UPDATE Notification SET status='".$status."' WHERE medicine_id='".$medicid."'AND patient_id='".$patient_id."'";
  if ($Connect->query($sql) === TRUE) {
 } else {
     echo "Error: " . $sql . "<br>" . $Connect->error;
